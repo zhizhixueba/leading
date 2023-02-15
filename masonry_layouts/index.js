6ea84f922db0439e3f46236ffa00fec5d16e46a7
知之学吧 <121071795+zzxbgit@users.noreply.github.com>
2023-02-15 11:58:24 +0800

    Create index.js

---
+++ masonry_layouts/index.js
@@ -0,0 +1,62 @@
+/**
+ * Author: Meng
+ * Date: 2023-02-10
+ * Desc: 瀑布流布局
+ */
+Page({
+  data: {
+    leftHeight: 0,
+    rightHeight: 0,
+    itemList: [], // { tag: 'left', type: 1, name: 0 }, // tag：left-左边；right-右边   type: 0黄色方格；1粉色方格
+  },
+
+  onLoad(options) {},
+
+  onReady() {
+    this._loadDatas();
+  },
+
+  onUnload() {},
+
+  onPullDownRefresh() {
+    this.setData({
+      itemList: [],
+    });
+    wx.stopPullDownRefresh();
+  },
+
+  onReachBottom() {
+    this._loadDatas();
+  },
+
+  _loadDatas() {
+    const that = this;
+
+    let leftHeight = that.data.leftHeight;
+    let rightHeight = that.data.rightHeight;
+    let itemList = [].concat(that.data.itemList);
+
+    for (let i = 0; i < 20; i++) {
+      const type = Math.random() * 10 > 5 ? 1 : 0;
+      let tag = '';
+
+      // 添加元素/计算高度
+      if (rightHeight > leftHeight) {
+        tag = 'right';
+        leftHeight += 240;
+        if (type == 1) {
+          rightHeight -= 20; // 计算高度
+        }
+      } else {
+        tag = 'left';
+        rightHeight += 240;
+        if (type == 1) {
+          leftHeight -= 20; // 计算高度
+        }
+      }
+      itemList.push({ tag, type, name: i});
+    }
+
+    that.setData({ itemList, rightHeight, leftHeight });
+  },
+});
