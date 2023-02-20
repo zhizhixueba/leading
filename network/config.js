cd1f9ac78a555134a0da98f413ff86a7fb36da15
知之学吧 <121071795+zzxbgit@users.noreply.github.com>
2023-02-20 14:27:17 +0800

    Add files via upload

---
+++ network/config.js
@@ -0,0 +1,43 @@
+/**
+ * Author: Meng
+ * Date: 2023-
+ * Desc: 网络请求配置
+ */
+
+// 常量配置
+export const app_config = {
+  env: 'prod', // prod: 生产, test: 测试, dev: 开发
+}
+
+// 获取环境域名
+export function getRequestHost(host = 'def', env = app_config.env) {
+  return env_hosts[env][host];
+}
+
+// 统一处理参数
+export function mergeParams(params = {}, url='') {
+  if(url.includes('store')) {
+    params.app = 'h5_store'
+  }
+  return params;
+}
+
+// 统一处理请求头
+export function mergeHeaders(headers = {}) {
+  headers.token = 'ASDIAH2H345K2HH32JH4KJ2K2J34_Test';
+  return headers;
+}
+
+// 环境服务域名配置
+const env_hosts = {
+  prod: {
+    def: 'http://def.com',
+    auth: 'http://auth.com',
+    order: 'http://order.com',
+  },
+  test: {
+    def: 'http://def-test.com',
+    auth: 'http://auth-test.com',
+    order: 'http://order-test.com',
+  }
+}
---
+++ network/fetch.js
@@ -0,0 +1,31 @@
+/**
+ * Author: Meng
+ * Date: 2023-
+ * Desc: 网络请求实体类
+ */
+
+// 普通请求
+export function network({ url, data, headers, method }) {
+  let body = null;
+  if (method == 'GET') {
+    url = _parseData(url, data)
+  } else {
+    body = JSON.stringify(data);
+  }
+  return fetch(url, { method, headers, body }).then(res => res.json());
+}
+
+// 下载
+export function download(path, option = {}) {
+  return fetch(path, option).then((data) => data.blob() || data.json());
+}
+
+function _parseData(url, params={}) {
+  console.log(params)
+  let list = []
+  for (const key in params) {
+    list.push(`${key}=${params[key]}`)
+  }
+  return `${url}?${list.join('&')}`
+}
+
